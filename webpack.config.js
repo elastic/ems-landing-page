84ea4332a81d66242c7e423bfdcaae6f5e46ddb9
Nick Peihl <nick.peihl@elastic.co>
2018-07-09 15:37:50 -0700

    Define babel presets only once

--- webpack.config.js
+++ webpack.config.js
@@ -32,9 +32,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
       }
     ]
   },
