ff3ab60026aca8322da1008e442d10d1c29622dc
Nick Peihl <nickpeihl@gmail.com>
2019-01-09 10:03:18 -0800

    [v2] Polyfill fetch for IE11 (#53) (#63)

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import URL from 'url-parse';
+import 'whatwg-fetch';
 import CONFIG from './config.json';
 import { App } from './js/components/app';
 import { ManifestParserV2 } from './js/manifest_parser_v2';
