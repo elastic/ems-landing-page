4b26c8069241543f5b9ac3e5668a234140495509
Nick Peihl <nickpeihl@gmail.com>
2019-01-09 10:04:21 -0800

    [v6.6] Add basemap attribution (#65)
    * Add basemap attribution

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -7,10 +7,14 @@
 import mapboxgl from 'mapbox-gl';
 import turfBbox from '@turf/bbox';
 import turfCenter from '@turf/center';
+import MarkdownIt from 'markdown-it';
 
 import React, { Component } from 'react';
 
-
+const markdownIt = new MarkdownIt({
+  html: false,
+  linkify: true
+});
 
 export class Map extends Component {
 
@@ -38,6 +42,7 @@ export class Map extends Component {
             tiles: [this.props.baseLayer.getUrlTemplate()],
             tileSize: 256,
             scheme: 'xyz',
+            attribution: markdownIt.render(this.props.baseLayer.attribution || '')
           },
         },
         layers: [{
