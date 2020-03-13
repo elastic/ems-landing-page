2f987f3e2c74c100b1337bf8a0e46c71366ee558
Nick Peihl <nick.peihl@elastic.co>
2020-03-13 13:14:20 -0700

    Add httpOnly parameter
    This parameter can be passed to yarn dev or yarn compile to skip injecting the client-side HTTPS redirect.

--- webpack.common.js
+++ webpack.common.js
@@ -12,82 +12,89 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const WebappWebpackPlugin = require('webapp-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = {
-  entry: {
-    mapbox: './node_modules/mapbox-gl/dist/mapbox-gl.js',
-    main: ['@babel/polyfill', path.resolve(__dirname, 'public/main.js')]
-  },
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'build/release'),
-    filename: '[name].bundle.js'
-  },
-  module: {
-    noParse: /iconv-loader\.js/,
-    rules: [
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'resolve-url-loader', 'postcss-loader', 'sass-loader'],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.hbs$/,
-        use: {
-          loader: 'handlebars-loader'
-        }
-      }
-    ]
-  },
-  externals: {
-    'config': JSON.stringify(require(path.resolve(__dirname, 'public', 'config.json')))
-  },
-  resolve: {
-    alias: {
-    }
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        exclude: /node_modules\/(?!mapbox-gl\/dist)/
-      })
-    ],
-    occurrenceOrder: true,
-    splitChunks: {
-      chunks: 'all'
+module.exports = env => {
+  return {
+    entry: {
+      mapbox: './node_modules/mapbox-gl/dist/mapbox-gl.js',
+      main: ['@babel/polyfill', path.resolve(__dirname, 'public/main.js')],
     },
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin(['./public/config.json']),
-    new WebappWebpackPlugin('@elastic/eui/lib/components/icon/assets/app_ems.svg'),
-    new HTMLWebpackPlugin({
-      template: 'public/index.hbs',
-      hash: true,
-      httpsRedirect: true,
-    }),
-    new OptimizeCssAssetsPlugin()
-  ],
-  stats: {
-    colors: true
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './build/release',
-    compress: true
-  }
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, 'build/release'),
+      filename: '[name].bundle.js',
+    },
+    module: {
+      noParse: /iconv-loader\.js/,
+      rules: [
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'resolve-url-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.hbs$/,
+          use: {
+            loader: 'handlebars-loader',
+          },
+        },
+      ],
+    },
+    externals: {
+      config: JSON.stringify(require(path.resolve(__dirname, 'public', 'config.json'))),
+    },
+    resolve: {
+      alias: {},
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          exclude: /node_modules\/(?!mapbox-gl\/dist)/,
+        }),
+      ],
+      occurrenceOrder: true,
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin(['./public/config.json']),
+      new WebappWebpackPlugin('@elastic/eui/lib/components/icon/assets/app_ems.svg'),
+      new HTMLWebpackPlugin({
+        template: 'public/index.hbs',
+        hash: true,
+        httpsRedirect: env && env.httpOnly ? false : true,
+      }),
+      new OptimizeCssAssetsPlugin(),
+    ],
+    stats: {
+      colors: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+      contentBase: './build/release',
+      compress: true,
+    },
+  };
 };
