7d61c8c859b2e2157964beee74e224586afe94a9
Jorge Sanz <jorge.sanz@elastic.co>
2020-11-19 11:40:56 +0100

    added target to header links

--- public/js/components/app.js
+++ public/js/components/app.js
@@ -184,10 +184,10 @@ export class App extends Component {
           </EuiHeaderSectionItem>
           <EuiHeaderSectionItem border="none">
             <EuiHeaderLinks gutterSize="xs">
-              <EuiHeaderLink iconType="logoElastic" href="https://elastic.co"> elastic.co </EuiHeaderLink>
-              <EuiHeaderLink iconType="logoGithub" href="https://www.github.com/elastic/ems-landing-page"> GitHub </EuiHeaderLink>
-              <EuiHeaderLink iconType="bug" href="https://www.github.com/elastic/ems-file-service/issues/new"> Report data issues </EuiHeaderLink>
-              <EuiHeaderLink iconType="documents" href="https://www.elastic.co/elastic-maps-service-terms"> Terms of Service </EuiHeaderLink>
+              <EuiHeaderLink target="_blank" iconType="logoElastic" href="https://elastic.co"> elastic.co </EuiHeaderLink>
+              <EuiHeaderLink target="_blank" iconType="logoGithub" href="https://www.github.com/elastic/ems-landing-page"> GitHub </EuiHeaderLink>
+              <EuiHeaderLink target="_blank" iconType="bug" href="https://www.github.com/elastic/ems-file-service/issues/new"> Report data issues </EuiHeaderLink>
+              <EuiHeaderLink target="_blank" iconType="documents" href="https://www.elastic.co/elastic-maps-service-terms"> Terms of Service </EuiHeaderLink>
             </EuiHeaderLinks>
           </EuiHeaderSectionItem>
         </EuiHeader>
