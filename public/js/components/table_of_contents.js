18c08f1b1ca167f66a96789384672a82e85119b0
Jorge Sanz <jorge.sanz@elastic.co>
2022-04-29 15:02:18 +0200

    Remove languages from side nav

--- public/js/components/table_of_contents.js
+++ public/js/components/table_of_contents.js
@@ -21,7 +21,6 @@ import { icon as EuiIconSearch } from '@elastic/eui/es/components/icon/assets/se
 import { icon as EuiIconArrowDown } from '@elastic/eui/es/components/icon/assets/arrow_down';
 import { icon as EuiIconArrowLeft } from '@elastic/eui/es/components/icon/assets/arrow_left';
 import { icon as EuiIconArrowRight } from '@elastic/eui/es/components/icon/assets/arrow_right';
-import { TMSService } from '@elastic/ems-client';
 
 // One or more icons are passed in as an object of iconKey (string): IconComponent
 appendIconComponentCache({
@@ -88,33 +87,9 @@ export class TableOfContents extends Component {
       });
       this.props.onTmsLayerSelect(config);
     }
-
-    if (id.startsWith('lang')) {
-      this.setState(() => {
-        return {
-          selectedLangId: id
-        };
-      });
-      this.props.onLanguageSelect(id);
-    }
   }
 
   _getSidebarItems() {
-    const Languages = TMSService.SupportedLanguages;
-
-    const langItems = Object.keys(Languages).map(langKey => {
-      const id = langKey;
-      const langId = `lang/${id}`;
-      const name = Languages[langKey].label;
-      return {
-        id,
-        name,
-        title: name,
-        isSelected: this.state.selectedLangId === langId,
-        onClick: () => this.selectItem(langId, id)
-      };
-    });
-
     const tmsItems = this.props.layers.tms.map((service) => {
       const id = `tms/${service.getId()}`;
       const name = service.getDisplayName();
@@ -139,22 +114,6 @@ export class TableOfContents extends Component {
       };
     });
 
-    const langs = {
-      id: 'langs',
-      name: 'Languages',
-      title: 'Languages',
-      icon: <EuiIcon type="globe" />,
-      items: [
-        {
-          id: 'select-lang',
-          name: 'Select a language',
-          title: 'Select a language',
-          onClick: undefined,
-          items: langItems
-        }
-      ]
-    };
-
     const tiles = {
       id: 'tms',
       name: 'Tile Layers',
@@ -171,6 +130,6 @@ export class TableOfContents extends Component {
       items: fileItems,
     };
 
-    return [tiles, langs, files];
+    return [tiles, files];
   }
 }
