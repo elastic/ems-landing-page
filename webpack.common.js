b96eedb20f963f97328ed344f43f2c31c93ede5c
Jorge Sanz <jorge.sanz@elastic.co>
2023-01-25 17:30:15 +0100

    prepare to upgrade to webpack 5

--- webpack.common.js
+++ webpack.common.js
@@ -71,7 +71,8 @@ module.exports = {
         exclude: /node_modules\/(?!maplibre-gl\/dist)/
       })
     ],
-    occurrenceOrder: true,
+    chunkIds: 'total-size',
+    moduleIds: 'size',
     splitChunks: {
       chunks: 'all'
     },
