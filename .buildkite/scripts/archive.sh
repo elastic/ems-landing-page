1a97ed528cd80d95ec5048f36177a07b6ed54e00
Jorge Sanz <jorge.sanz@elastic.co>
2023-04-21 14:30:59 +0200

    Refactor code to simplify the process

--- .buildkite/scripts/archive.sh
+++ .buildkite/scripts/archive.sh
@@ -1,4 +1,4 @@
-#
+#!/usr/bin/env bash
 # Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
 # or more contributor license agreements. Licensed under the Elastic License;
 # you may not use this file except in compliance with the Elastic License.
@@ -7,37 +7,25 @@
 set -e
 set +x
 
-# Checking for the parameters
-
-if [[ -z "${STAGING_BUCKET}" ]]; then
-    echo "--- :fire:  STAGING_BUCKET is not set, e.g. 'STAGING_BUCKET=elastic-ems-dev-maps-landing-page-staging'" 1>&2
-    exit 1
-fi
-
-if [[ -z "${PRODUCTION_BUCKET}" ]]; then
-    echo "--- :fire:  PRODUCTION_BUCKET is not set, e.g. 'PRODUCTION_BUCKET=elastic-ems-dev-maps-landing-page-live'" 1>&2
-    exit 1
-fi
-
-if [[ -z "${ARCHIVE_BUCKET}" ]]; then
-    echo "--- :fire:  ARCHIVE_BUCKET is not set, e.g. 'ARCHIVE_BUCKET=elastic-ems-dev-maps-landing-page-staging'" 1>&2
-    exit 1
-fi
+# Parameters:
+# - STAGING_BUCKET
+# - ARCHIVE_BUCKET
 
 
-TIMESTAMP=`date +"%Y-%m-%d_%H-%M-%S"`
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 SNAPSHOT_DIR=$PWD/${TIMESTAMP}_snapshot
 ZIP_FILE=${TIMESTAMP}_ems_landingpages.tar.gz
 ZIP_FILE_PATH=$PWD/$ZIP_FILE
 
+
 echo "--- :arrow_down: Copying gs://$STAGING_BUCKET to $SNAPSHOT_DIR"
+
 if [[ -d "$SNAPSHOT_DIR" ]]; then
     echo ":fire: $SNAPSHOT_DIR already exist" 1>&2
     exit 1
 fi
 mkdir -p "$SNAPSHOT_DIR"
 gsutil -m cp -r "gs://$STAGING_BUCKET/*" "$SNAPSHOT_DIR"
-
 
 echo "--- :compression: Archiving assets into $ZIP_FILE_PATH"
 tar -czvf "$ZIP_FILE_PATH" -C "$SNAPSHOT_DIR" .
@@ -52,12 +40,10 @@ set -e
 echo "--- :file_cabinet: Copying $ZIP_FILE_PATH snapshot to gs://$ARCHIVE_BUCKET"
 gsutil cp "$ZIP_FILE_PATH" "gs://$ARCHIVE_BUCKET"
 
+
 set +e
 if ! gsutil -q stat "gs://$ARCHIVE_BUCKET/$ZIP_FILE" ; then
     echo ":fire: ERROR: snapshot file \"gs://$ARCHIVE_BUCKET/$ZIP_FILE\" did not upload successfully" 1>&2
     exit 1
 fi
 set -e
-
-echo "--- :gcloud: Syncing gs://$STAGING_BUCKET to gs://$PRODUCTION_BUCKET"
-gsutil -m rsync -d -r -a public-read "gs://$STAGING_BUCKET" "gs://$PRODUCTION_BUCKET"
---
+++ .buildkite/scripts/upload.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
+set -e
+set +x
+
+echo "--- :compression: Downloading and uncompressing the build"
+buildkite-agent artifact download release.tar.gz .
+tar -xvzf release.tar.gz
+
+if [[ ! -d ./build/release ]]; then
+    echo "--- :fire:  There is no release to upload" 1>&2
+    exit 1
+fi
+
+SOURCE_PATH="./build/release/"
+
+case ${EMS_ENVIRONMENT} in
+    "staging")
+        DEST_BUCKET="gs://${STAGING_BUCKET}"
+    ;;
+    "production")
+        DEST_BUCKET="gs://${PRODUCTION_BUCKET}"
+    ;;
+    "*")
+        echo "--- :fire: ${EMS_ENVIRONMENT}  is not a valid environment definition" 1>&2
+        exit 1
+esac
+
+DEST_PATH="${DEST_BUCKET}/${BUILDKITE_BRANCH}/"
+
+
+# The trailing slash is critical with the branch.
+# Otherwise, files from subdirs will go to the same destination dir.
+echo "--- :gcloud: Sync ${SOURCE_PATH} to ${DEST_PATH}"
+gsutil -m rsync -d -r -a public-read -j js,css,html "${SOURCE_PATH}" "${DEST_PATH}" 
+
+# If the branch name matches ROOT_BRANCH, it also gets synced to the root
+# excluding paths matching other versions to avoid removing them (v2, v7.x, etc.)
+if [[ "$BUILDKITE_BRANCH" == "$ROOT_BRANCH" ]]; then
+    echo "--- :gcloud: Sync ${SOURCE_PATH} to ${DEST_BUCKET}"
+    gsutil -m rsync -d -r -a public-read -j js,css,html  -x '^v[\d.]+\/.*$' "${SOURCE_PATH}" "${DEST_BUCKET}/"
+fi
