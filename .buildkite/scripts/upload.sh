cfcab841d8558d4b20f845f0d3d4ec4f3fec27b4
Jorge Sanz <jorge.sanz@elastic.co>
2023-05-05 12:25:58 +0200

    [v7.17] Buildkite deploy (#552) | Fix the branch check to stage and deploy (#575) (#579)
    * Buildkite deploy (#552)
    
    * A first version deploying staging
    
    * Fix scripts location
    
    * Fix script location 🤦
    
    * Adding a check for buildkite modifying a script 🤔, forgot to get the GCP creds from vault
    
    * changing permissions to scripts so git-check.sh does not complain
    
    * Fix artifact upload file name in pipeline
    
    * Forgot to export a variable 🤦
    
    * Adding verbosity to the artifact uncompressing
    
    * Adding the correct path for the release
    
    * Moved logic to the pre-command hook, added a deploy step for tagged commits
    
    * joining lines on step if condition
    
    * converting the if to a regex, commenting the production section for now
    
    * commenting staging, testing the deploy to production script
    
    * Changing labels and setting the increased resources only for the build step
    
    * remove redundant image name and the temporal branch reference
    
    * Disable Jenkins deploy scripts
    
    * Document dependencies of this component
    
    * Refactor code to simplify the process
    
    * Set exec permission to archive.sh script
    
    * Restore the if statement for the deploy step
    
    * Switch tag condition to test the deploy execution branch
    
    * Remove the github user name from the buildkite branch
    
    * More concise messages for archive log
    
    * set up PR branch as root for testing
    
    * Ensure the comparison for root is aganist branch name without user
    
    * Restored production values and added a bit more of documentation
    
    * More generic regex for whitelisted branches
    
    * ifs in one line and remove start and ending regex chars
    
    * restore a removed &&
    
    * Pin the agent image
    
    * Feedback: Setting 8.7 as the root branch
    
    (cherry picked from commit cb3fece5d96842db71fefd14dec2c80f0a3b8326)
    
    * Fix the branch check to stage and deploy (#575)
    
    (cherry picked from commit 6681a62c821fcbaef6fdc2e9de2ec244bd723924)

---
+++ .buildkite/scripts/upload.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
+set -e
+set +x
+
+# Script to upload the result of a build into a GCP bucket, also synchronizing
+# the release with the root of the bucket if the branch of the build is the same
+# as the root branch set up in the pre-command script.
+#
+# Parameters:
+# - BUILDKITE_BRANCH: the branch from which this build was triggered
+# - EMS_ENVIRONMENT: defines if this is going to be a staging or production release
+# - STAGING_BUCKET
+# - PRODUCTION_BUCKET
+# - ROOT_BRANCH
+
+echo "--- :compression: Downloading and uncompressing the build"
+buildkite-agent artifact download release.tar.gz .
+tar -xvzf release.tar.gz
+
+if [[ ! -d ./build/release ]]; then
+    echo "--- :fire:  There is no release to upload" 1>&2
+    exit 1
+fi
+
+SOURCE_PATH="./build/release/"
+
+# Define the destination of the release
+case ${EMS_ENVIRONMENT} in
+    "staging")
+        DEST_BUCKET="gs://${STAGING_BUCKET}"
+    ;;
+    "production")
+        DEST_BUCKET="gs://${PRODUCTION_BUCKET}"
+    ;;
+    "*")
+        echo "--- :fire: ${EMS_ENVIRONMENT}  is not a valid environment definition" 1>&2
+        exit 1
+esac
+
+BRANCH="${BUILDKITE_BRANCH#*:}"
+DEST_PATH="${DEST_BUCKET}/${BRANCH}/"
+
+
+# The trailing slash is critical with the branch.
+# Otherwise, files from subdirs will go to the same destination dir.
+echo "--- :gcloud: Sync ${SOURCE_PATH} to ${DEST_PATH}"
+gsutil -m rsync -d -r -a public-read -j js,css,html "${SOURCE_PATH}" "${DEST_PATH}" 
+
+# If the branch name matches ROOT_BRANCH, it also gets synced to the root
+# excluding paths matching other versions to avoid removing them (v2, v7.x, etc.)
+if [[ "$BRANCH" == "$ROOT_BRANCH" ]]; then
+    echo "--- :gcloud: Sync ${SOURCE_PATH} to ${DEST_BUCKET}"
+    gsutil -m rsync -d -r -a public-read -j js,css,html  -x '^v[\d.]+\/.*$' "${SOURCE_PATH}" "${DEST_BUCKET}/"
+fi
