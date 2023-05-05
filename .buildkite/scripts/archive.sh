5f4f6450a90f693da161f79bcca362119a290f97
Jorge Sanz <jorge.sanz@elastic.co>
2023-05-05 11:34:47 +0200

    Buildkite deploy (#552)
    * A first version deploying staging
    
    * Fix scripts location
    
    * Fix script location 🤦
    
    * Adding a check for buildkite modifying a script 🤔, forgot to get the GCP creds from vault
    
    * changing permissions to scripts so git-check.sh does not complain
    
    * Fix artifact upload file name in pipeline
    
    * Forgot to export a variable 🤦
    
    * Adding verbosity to the artifact uncompressing
    
    * Adding the correct path for the release
    
    * Moved logic to the pre-command hook, added a deploy step for tagged commits
    
    * joining lines on step if condition
    
    * converting the if to a regex, commenting the production section for now
    
    * commenting staging, testing the deploy to production script
    
    * Changing labels and setting the increased resources only for the build step
    
    * remove redundant image name and the temporal branch reference
    
    * Disable Jenkins deploy scripts
    
    * Document dependencies of this component
    
    * Refactor code to simplify the process
    
    * Set exec permission to archive.sh script
    
    * Restore the if statement for the deploy step
    
    * Switch tag condition to test the deploy execution branch
    
    * Remove the github user name from the buildkite branch
    
    * More concise messages for archive log
    
    * set up PR branch as root for testing
    
    * Ensure the comparison for root is aganist branch name without user
    
    * Restored production values and added a bit more of documentation
    
    * More generic regex for whitelisted branches
    
    * ifs in one line and remove start and ending regex chars
    
    * restore a removed &&
    
    * Pin the agent image
    
    * Feedback: Setting 8.7 as the root branch
    
    (cherry picked from commit cb3fece5d96842db71fefd14dec2c80f0a3b8326)

---
+++ .buildkite/scripts/archive.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
+set -e
+set +x
+
+# This script stores a full copy of the EMS Landing Page
+# production bucket into a zip file and uploads it to
+# an archive bucket
+#
+# Parameters:
+# - STAGING_BUCKET
+# - ARCHIVE_BUCKET
+
+
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+SNAPSHOT_DIR=$PWD/${TIMESTAMP}_snapshot
+ZIP_FILE=${TIMESTAMP}_ems_landingpages.tar.gz
+ZIP_FILE_PATH=$PWD/$ZIP_FILE
+
+
+echo "--- :arrow_down: Downloading gs://$STAGING_BUCKET to snapshot"
+
+if [[ -d "$SNAPSHOT_DIR" ]]; then
+    echo ":fire: $SNAPSHOT_DIR already exist" 1>&2
+    exit 1
+fi
+mkdir -p "$SNAPSHOT_DIR"
+gsutil -m cp -r "gs://$STAGING_BUCKET/*" "$SNAPSHOT_DIR"
+
+echo "--- :compression: Archiving assets into $ZIP_FILE"
+tar -czvf "$ZIP_FILE_PATH" -C "$SNAPSHOT_DIR" .
+
+set +e
+if gsutil -q stat "gs://$ARCHIVE_BUCKET/$ZIP_FILE" ; then
+    echo "--- :fire: ERROR: snapshot file \"gs://$ARCHIVE_BUCKET/$ZIP_FILE\" already exists" 1>&2
+    exit 1
+fi
+set -e
+
+echo "--- :file_cabinet: Uploading snapshot to gs://$ARCHIVE_BUCKET"
+gsutil cp "$ZIP_FILE_PATH" "gs://$ARCHIVE_BUCKET"
+
+
+set +e
+if ! gsutil -q stat "gs://$ARCHIVE_BUCKET/$ZIP_FILE" ; then
+    echo ":fire: ERROR: snapshot file \"gs://$ARCHIVE_BUCKET/$ZIP_FILE\" did not upload successfully" 1>&2
+    exit 1
+fi
+set -e
