3fdfb10f5478f8639847345b501d3529b711dfe6
Jorge Sanz <jorge.sanz@elastic.co>
2025-05-28 19:33:10 +0200

    Add globe projection

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -12,6 +12,8 @@ import turfCenter from '@turf/center';
 import React, { Component } from 'react';
 import chroma from 'chroma-js';
 
+import { eui } from './theme';
+
 maplibre.setRTLTextPlugin(mbRtlPlugin);
 
 export class Map extends Component {
@@ -38,11 +40,13 @@ export class Map extends Component {
     this._overlayFillHighlightId = 'overlay-fill-highlight-layer';
     this._tmsSourceId = 'vector-tms-source';
     this._tmsLayerId = 'vector-tms-layer';
+    this._mapRef = React.createRef();
+    this._colorMode = props.colorMode || 'light';
   }
 
   componentDidMount() {
     this._maplibreMap = new maplibre.Map({
-      container: this.refs.mapContainer,
+      container: this._mapRef.current,
       style: {
         version: 8,
         sources: {},
@@ -54,6 +58,7 @@ export class Map extends Component {
     });
 
     this._maplibreMap.addControl(new maplibre.FullscreenControl());
+    this._maplibreMap.addControl(new maplibre.GlobeControl());
 
     this._maplibreMap.dragRotate.disable();
     this._maplibreMap.touchZoomRotate.disableRotation();
@@ -182,6 +187,34 @@ export class Map extends Component {
     // We must persist the overlay layers and overlay source by creating a new style from
     // the incoming source and the overlay layers.
     const newStyle = this._persistOverlayLayers(source);
+    
+    const prevStyle = this._maplibreMap.getStyle();
+
+    // Switch to mercator if previous style was mercator
+    if (prevStyle.projection && prevStyle.projection.type === 'mercator') {
+      newStyle.projection = prevStyle.projection;
+    } else {
+      newStyle.projection = { type: 'globe' };
+    }
+
+    // Set backgrounpd color depending on the style name
+    // taking the EUI token based on the color mode
+    if (source) {
+      const styleName = newStyle.name || '';
+      const colorMode = this._colorMode == 'light' ? 'LIGHT' : 'DARK';
+
+      const backgroundColor = styleName.startsWith('Dark')
+        ? eui.theme.colors['LIGHT'].darkestShade
+        : styleName.startsWith('Light')
+          ? eui.theme.colors[colorMode].lightShade
+          : eui.theme.colors[colorMode].lightestShade;
+
+      if (this._mapRef.current && this._mapRef.current.style) {
+        this._mapRef.current.style.backgroundColor = backgroundColor;
+      }
+    }
+    
+
 
     this._maplibreMap.setStyle(newStyle, { diff: false });
 
@@ -260,6 +293,6 @@ export class Map extends Component {
   }
 
   render() {
-    return (<div className="mapContainer" ref="mapContainer" />);
+    return (<div className="mapContainer" ref={this._mapRef} />);
   }
 }
